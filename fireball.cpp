cecf208b6d99fe99c61715662d38debf9e45cb58
MrSKuD <51855486+MrSKuD@users.noreply.github.com>
2020-05-10 11:11:23 +0200

    Update fireball.cpp

--- fireball.cpp
+++ fireball.cpp
@@ -1,3 +1,9 @@
+/*
+@Author: Enzo MOULIN
+@Website: http://www.enzomoulin.com/
+@Email: enzo.moulin@isen-ouest.yncrea.fr
+ */
+
 #include "fireball.h"
 
 FireBall::FireBall(QString imageFileName) : QGraphicsPixmapItem(QPixmap(imageFileName)) {
